883b037e31b9152bd3e17a118516dcdb029a4963
Paul Morris <pam5089@gmail.com>
2017-03-13 01:58:22 +0000

    Added some shitty angle calculations.

--- Demo1/sign_detect.py
+++ Demo1/sign_detect.py
@@ -3,16 +3,21 @@ from utils import * # functions for getting the canny image
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import math
 
 # constants
 RESOLUTION = (1020, 768)
-STOP_TEMPLATE = cv2.imread("images/Stop.jpg", 1)
+STOP_TEMPLATE = cv2.imread("images/Stop.png", 1)
 STOP_POINTS = [(217, 245), (217, 800), (800, 161), (800, 485), (1105, 326)]
 LEFT_TEMPLATE = cv2.imread("images/Turn_Left.png", 1)
 LEFT_POINTS = [(480, 161), (480, 488), (175, 326), (217, 800), (1064, 242), (1062, 804)]
 RIGHT_TEMPLATE = cv2.imread("images/Turn_Right.png", 1)
 RIGHT_POINTS = [(217, 245), (217, 800), (800, 161), (800, 485), (1105, 326)]
 SIGN_DIM = (1280, 962)
+
+# for the tracking vector
+prev_image = -1 
+prev_points = -1
 
 def matches_template(warped, template):
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -45,6 +50,7 @@ def determine_sign(image, canny):
     #draw_contours(image, approx)
     #draw_contours(image, approx)
     sign = "None"
+    angle = 0
     points = []
     if (len(np.squeeze(approx)) >= 4):
         # flatten the image
@@ -53,42 +59,78 @@ def determine_sign(image, canny):
         # convert to black and white 
         warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
         ret, warped = cv2.threshold(warped, 90, 255, cv2.THRESH_BINARY) 
-    
+   
         # compare against the templates
         if (matches_template(warped, STOP_TEMPLATE)):
             sign = "Stop"
             points = STOP_POINTS
+            SIGN_DIM  = [100, 100]
         if (matches_template(warped, LEFT_TEMPLATE)):
             sign = "Left"
             points = LEFT_POINTS
         elif (matches_template(warped, RIGHT_TEMPLATE)):
             sign = "Right"
             points = RIGHT_POINTS
-            draw_points(RIGHT_TEMPLATE, RIGHT_POINTS)
         #display_image("canny", warped)
         #draw_contours(image, approx)
 
-        # scale important lists to match scaled image size
-        live_dim = (warped.shape[1], warped.shape[0])
-        scale = (float(SIGN_DIM[0]) / live_dim[0], float(SIGN_DIM[1]) / live_dim[1])
-        points = scale_points(scale, points)
+        # get area of sign in image
+        area = cv2.contourArea(approx)
 
-        # test matrix
-        print "M", M
+        # get center of sign in image
+        approx = np.squeeze(approx)
+        center = ((approx[0][0] + approx[1][0]) / 2, (approx[0][1] + approx[1][1]) / 2)
+        points = [center, (0, 0)]
+#
+#        # scale important lists to match scaled image size
+#        live_dim = (warped.shape[1], warped.shape[0])
+#        scale = (float(SIGN_DIM[0]) / live_dim[0], float(SIGN_DIM[1]) / live_dim[1])
+#        points = scale_points(scale, points)
+#
+#        # change points to be four corners
+#        points = [(0, 0), (0, warped.shape[0]), (warped.shape[1], 0), (warped.shape[1], warped.shape[0])]
+#
+#        # test matrix
+#        #draw_points(warped, points)
+#
+#        # map the points to the actual captured image
+#        M[1][1] += 10^-6 # to avoid a singular matrix error
+#        inverse = np.linalg.inv(M)
+#        result = []
+#        for point in points:
+#            point = [point[0], point[1], 1] # convert points to 3D
+#            point = (np.asarray(point)).reshape(3, 1)
+#            new_point = (np.dot(inverse, point))
+#            #new_point = np.transpose(new_point)
+#            #new_point = [p / new_point[2] for p in new_point]    # normalize z
+#            result.append((new_point[0][0] / new_point[2][0], -1*new_point[1][0] / new_point[2][0]))
+#            print "result", result
+#        draw_points(image, center)
+#
+#        # get the optical vector or w/e
+#        if (prev_image != -1 and prev_points != -1):
+#            cv2.calcOpticalFlowPyrLK(prev_image, image, prev_points, points)
+#            prev_image = image
+#            prev_points = points
+#        else:
+#            prev_image = image
+#            prev_points = points
+        
+        # find only the good points
+        #cv2.findHomography()
 
-        # map the points to the actual captured image
-        M[1][1] += 10^-6 # to avoid a singular matrix error
-        inverse = np.linalg.inv(M)
-        result = []
-        for point in points:
-            point = (point[0], point[1], 0) # convert points to 3D
-            point = np.asarray(point)
-            new_point = np.transpose(np.dot(inverse, point))
-            result.append((new_point[0], new_point[1]))
-        print "points on pic", result
-        draw_points(image, result)
-       
-    return sign
+        # find approximate distance of sign from robot
+        # just guess until you get something right
+        distance = float(500) / 148000 * area
+
+        # find angle
+        image_center = (RESOLUTION[0] / 2, RESOLUTION[1] / 2)
+        difference = center[0] - image_center[0]    # just need x-diff
+        if (distance == 0):
+            distance = 1
+        angle = math.degrees(math.atan(float(difference) / distance))
+
+    return angle, sign
 
 def draw_points(image, points):
     # draw dots on image
@@ -114,18 +156,16 @@ filename = "temp.jpg"
 # main loop
 while (True): 
     # get the image
-    #camera.capture(filename)
-    filename = str(sys.argv[1])
+    camera.capture(filename)
+    #filename = str(sys.argv[1])
     image = cv2.imread(filename)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # detect the sign type
     canny = get_canny(grey, hsv)
-    sign = determine_sign(image, canny)
-    
+    angle, sign = determine_sign(image, canny)
+    print sign, angle    
     # calculate the angle
 
-    # print out the sign currently displayed 
-    print sign
 
